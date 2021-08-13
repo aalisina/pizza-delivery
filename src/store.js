33726fe963365dc3fe9542781dfdbbb76c68ec3a
sinazay123 <sinazay123@gmail.com>
2021-08-13 17:25:23 -0500

    agregar store de redux

---
+++ src/store.js
@@ -0,0 +1,16 @@
+import { combineReducers } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { getAllPizzasReducers } from "./reducers/pizzaReducers";
+getAllPizzasReducers
+
+const finalReducer = combineReducers({
+    getAllPizzasReducers: getAllPizzasReducers
+})
+const initialState = {};
+const composeEnhancers = composeWithDevTools({});
+
+const store = createStore(finalReducer, initialState, composeEnhancers(applyMiddleware(thunk)));
+
+export default store;
