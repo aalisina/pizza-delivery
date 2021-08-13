c23cdfb1a9b9175c120286b4adbf1fadf7e071af
sinazay123 <sinazay123@gmail.com>
2021-08-13 10:48:06 -0500

    agregar modal al componente de pizza

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react'
+import { Modal } from 'react-bootstrap'
 
 function Pizza({pizza}) {
     const [quantity, setQuantity] = useState(1);
-    const [variant, setVariant] = useState('small')
+    const [variant, setVariant] = useState('small');
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
+        
     <div style={{margin: '80px'}} className="shadow-lg p-3 mb-5 bg-white rounded">
-        <h1>{pizza.name}</h1>
-        <img src={pizza.image} className="img-fluid" style={{height: '200px', width:'200px'}} />
+        <div onClick={handleShow} >
+            <h1>{pizza.name}</h1>
+            <img src={pizza.image} className="img-fluid" style={{height: '200px', width:'200px'}} alt="" />
+        </div>
         <div className="flex-container">
             <div className="w-100 m-1">
                 <p>Variants</p>
@@ -42,6 +51,21 @@ function Pizza({pizza}) {
                     <button className="btn ">Add To Cart</button>
             </div>
         </div>
+
+                <Modal show={show}>
+                    <Modal.Header>
+                        <Modal.Title>{pizza.name}</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <img src={pizza.image} alt="" className="img-fluid" style={{height: '400px'}} />
+                        <p>{pizza.description}</p>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <button className="btn" onClick={handleClose}>Close</button>
+                    </Modal.Footer>
+            </Modal>
     </div>
     )
 }
