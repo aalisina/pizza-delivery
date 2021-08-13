14d5e83479def1144a45a6791af352202e21c923
sinazay123 <sinazay123@gmail.com>
2021-08-12 19:26:51 -0500

    agregar buttons y usestate para 2 variables

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Pizza({pizza}) {
+    const [quantity, setQuantity] = useState(1);
+    const [variant, setVariant] = useState('small')
     return (
     <>
         <h1>{pizza.name}</h1>
@@ -8,7 +10,9 @@ function Pizza({pizza}) {
         <div className="flex-container">
             <div className="w-100">
                 <p>Variants</p>
-                <select >
+                <select className="form-control" value={variant} onChange={ (e)=> {
+                    setVariant(e.target.value)
+                }}>
                  {pizza.varients.map(variant => {
                         return <option value={variant}>{variant}</option>
                     })}
@@ -18,13 +22,25 @@ function Pizza({pizza}) {
 
             <div className="w-100">
                 <p>Quantity</p>
-                <select >
+                <select className="form-control" value={quantity} onChange={(e)=> {
+                    setQuantity(e.target.value)
+                }}>
                     {[...Array(10).keys()].map((x, i)=> {
                         return <option value={i+1}>{i+1}</option>
                     })}
                 </select>
             </div>
 
+        </div>
+
+        <div className="flex-container">
+            <div className="m-1 w-100">
+                    <h1>Price: {pizza.prices[0][variant] * quantity }</h1>
+            </div>
+
+            <div className="m-1 w-100">
+                    <button className="btn ">Add To Cart</button>
+            </div>
         </div>
     </>
     )
